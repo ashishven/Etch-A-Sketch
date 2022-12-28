11dc9ce21ce95544935f75b0f30bbcd9dfacc116
Ashish <113494198+svenkiah6@users.noreply.github.com>
2022-12-28 21:31:29 +0400

    Add new grid functionality

--- script.js
+++ script.js
@@ -1,33 +1,61 @@
+createGrid(4)
 
+const newGridBtn = document.querySelector('button')
 
-let container = document.createElement("div")
-container.classList.add("container")
+newGridBtn.addEventListener('click', e=>{
 
-for(let i =0;i<24;i++)
-{
-    let newRow = document.createElement("div")
-    newRow.classList.add("row")
-
-    for(let j =0;j<24;j++)
-    {
-       let newSquare = document.createElement("div") 
-       newSquare.classList.add("square")
-       newRow.appendChild(newSquare)
-    }
-    container.append(newRow)
-}
-
-document.body.appendChild(container)
-
-squares = document.querySelectorAll(".square")
-
-squares.forEach(square => {
-
-    square.addEventListener("mouseover",e =>{
-        square.style.backgroundColor="black";
-
-    });
+    let side = prompt("Enter number of squares on each side")
+    let container = document.querySelector('.container')
+    container.remove()
+    createGrid(side)
 
 });
 
-function createGrid()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function createGrid(side) {
+    let container = document.createElement("div")
+    container.classList.add("container")
+
+    for (let i = 0; i < side; i++) {
+        let newRow = document.createElement("div")
+        newRow.classList.add("row")
+
+        for (let j = 0; j < side; j++) {
+            let newSquare = document.createElement("div")
+            newSquare.classList.add("square")
+            newRow.appendChild(newSquare)
+        }
+        container.append(newRow)
+    }
+
+
+    document.body.appendChild(container)
+
+    squares = document.querySelectorAll(".square")
+
+    squares.forEach(square => {
+
+        square.addEventListener("mouseover", e => {
+            square.style.backgroundColor = "black";
+
+        });
+
+    });
+}
